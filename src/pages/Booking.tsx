ec5ccbf55f4bfcf44390f0934eb5e40771d2eecb
jonathanng1122 <jonathanng1122@gmail.com>
2022-01-17 20:52:34 -0600

    fixing booking erros

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -161,7 +161,6 @@ const BookingPage = () => {
         <PageGrid>
           <ImagesColumnContainer>
             {loaded && creator && creator.demos && <ImagesSlider images={creator.demos} />}
-            <ImagesSliderLoading style={{ width: '100%', height: 460 }} />
           </ImagesColumnContainer>
           <RightPanel creator={creator} account={account} loaded={loaded}>
             {(creator, account) => (
@@ -290,7 +289,7 @@ const BookingPage = () => {
                       <PrimaryButton
                         onPress={async () => {
                           setLoading(true);
-                          await validateForm();
+                          const errors = await validateForm();
                           if (Object.keys(errors).length != 0) {
                             toast.error('Please fix the errors.');
                           } else {
