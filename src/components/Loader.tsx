75305f7678d77c3061e6aed506a7db136a229ea7
CarbonEdge <wholetree65@gmail.com>
2022-01-29 11:37:11 +0200

    reduce loader size

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -15,7 +15,7 @@ const LoaderDiv = styled.div`
 const Loader = () => {
   return (
     <LoaderDiv>
-      <ReactLoading type={'spin'} color="#fff" height={667} width={375} />
+      <ReactLoading type={'spin'} color="#fff" height={400} width={200} />
     </LoaderDiv>
   );
 };
