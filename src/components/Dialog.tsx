8adb74b53473252e0ac7fe54a9d5fa62604453b8
rushikesh <rushikeshdhanwant@gmail.com>
2022-03-03 14:56:33 +0530

    formatted files

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -139,7 +139,7 @@ const DropDown: React.FC<DropDownProps> = (props) => {
     <div
       style={{
         position: 'absolute',
-        right:0,
+        right: 0,
         background: 'rgba(0, 0, 0, 0.5)',
         display: 'flex',
         alignItems: 'center',
