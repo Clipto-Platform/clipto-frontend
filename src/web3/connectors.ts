af021d4fa7e2cdb25b3193a1f11f4c495b11181f
rushikesh <rushikeshdhanwant@gmail.com>
2022-03-15 15:01:27 +0530

    updated featuured list and wallet connect rpc

--- src/web3/connectors.ts
+++ src/web3/connectors.ts
@@ -1,15 +1,15 @@
 import { InjectedConnector } from '@web3-react/injected-connector';
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
-
+import { DEFAULT_CHAIN_ID , RPC_URLS } from '../config/config'
 export const injected = new InjectedConnector({
   // allow any chain to 'connect' so we can help redirect them
 });
 
 export const walletconnect = new WalletConnectConnector({
-  supportedChainIds: [80001],
-  chainId: 80001,
+  supportedChainIds: [137],
+  chainId: 137,
   rpc: {
-    80001: 'https://polygon-mumbai.g.alchemy.com/v2/VMBpFqjMYv2w-MWnc9df92w3R2TpMvSG',
+    137:'https://polygon-mainnet.g.alchemy.com/v2/Wk4fc10DkXi2lhLq30tw_eSHPuzUyRnV',
   },
   qrcode: true,
   clientMeta: {
