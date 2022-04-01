807b142d9b53a74c92ab3f417b34567adec6492c
rushikesh <rushikeshdhanwant@gmail.com>
2022-04-01 17:16:03 +0530

    updated featured creators for test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,6 +6,7 @@ import { HeaderContentGapSpacer, HeaderSpacer } from '../../components/Header/He
 import { ContentWrapper, PageContentWrapper, PageWrapper } from '../../components/layout/Common';
 import { UserDisplay } from '../../components/UserDisplay/UserDisplay';
 import { HeroTitle } from './Style';
+import { TEST } from '../../config/config';
 
 const featuredList: string[] = [
   '0xCFFE08BDf20918007f8Ab268C32f8756494fC8D8', // Gabriel Haines.eth
@@ -14,12 +15,19 @@ const featuredList: string[] = [
   '0x0c44cb8087a269e7cc1f416a9bb4d5e9fed4eb9f', // bobburnquist
   '0x1c6f1a832e73949c97fe335a98b6a5fc3c9c29e9', // mackrypto
 ];
-
+const featuredListTest: string[] = [
+  '0x7c98c2dec5038f00a2cbe8b7a64089f9c0b51991', // atul
+  '0x8528f9805a82158c22814bda63015537a5e670c3', // rushi
+  '0x7cacbc75d74740b50dc68fbf0a573af80243ca56', // jon
+  '0x6e4cd1a58e0d1309da36f1ce1e456e5b93483175', // lee
+];
 const HomePage = () => {
   let [creators, setCreators] = useState<EntityCreator[]>([]);
 
   useEffect(() => {
-    const creatorAddresses = featuredList.map((c) => c.toLowerCase());
+    const creatorAddresses = TEST
+      ? featuredListTest.map((c) => c.toLowerCase())
+      : featuredList.map((c) => c.toLowerCase());
 
     featuredCreators(creatorAddresses).then((response) => {
       if (response.data) {
