896808a450eeffc1c3b937a3d71eea4b0311501e
rushikesh <rushikeshdhanwant@gmail.com>
2022-04-11 18:11:05 +0530

    added dropdown in booking form

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,29 +1,23 @@
-import React from "react";
-import {
-  DropdownWrapper,
-  StyledSelect,
-  StyledOption,
-  StyledLabel,
-} from "./styles";
+import React from 'react';
+import Icon from '../../assets/svgs/dropdown.svg';
+import { DropdownWrapper, StyledSelect, StyledOption, StyledLabel, StyledSelectDiv } from './styles';
 
-export function Dropdown(props:any) {
+export function Dropdown(props: any) {
   return (
-    <DropdownWrapper 
-    onChange={props.onChange}>
-      <StyledLabel htmlFor="token">
-        {props.formLabel}
-      </StyledLabel>
-      <StyledSelect id="token" name="token">
-        {props.children}
-      </StyledSelect>
+    <DropdownWrapper onChange={props.onChange}>
+      <StyledLabel htmlFor="token">{props.formLabel}</StyledLabel>
+      <StyledSelectDiv>
+        <StyledSelect id="token" name="token">
+          {props.children}
+        </StyledSelect>
+        <div>
+          <img src={Icon} alt="icon" />
+        </div>
+      </StyledSelectDiv>
     </DropdownWrapper>
   );
 }
 
-export function Option(props:any) {
-  return (
-    <StyledOption selected={props.selected}>
-      {props.value}
-    </StyledOption>
-  );
+export function Option(props: any) {
+  return <StyledOption selected={props.selected}>{props.value}</StyledOption>;
 }
--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const DropdownWrapper = styled.div`
   display: flex;
@@ -6,45 +6,49 @@ export const DropdownWrapper = styled.div`
   justify-content: flex-start;
 `;
 
-export const StyledSelect = styled.select`
+export const StyledSelectDiv = styled.div`
   max-width: 220px;
   height: 100%;
   padding: 0.5rem;
   margin-bottom: 1rem;
+  border: 1px solid #2a2a2a;
+  box-sizing: border-box;
+  border-radius: 8px;
+  min-height: 48px;
+  padding-left: 12px 16px;
+  padding-right: 12px;
+  min-width: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  position: realtive;
+`;
+export const StyledSelect = styled.select`
+  background: transparent;
+  height: 100%;
   font-family: 'Scto Grotesk A';
-    background: transparent;
-    border: 1px solid #2A2A2A;
-    box-sizing: border-box;
-    border-radius: 8px;
-    min-height: 48px;
-    outline: none;
-    padding-left: 16px;
-    padding-right: 12px;
-    min-width: 0;
-    width: 100%;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 16px;
-    line-height: 20px;
-    color: #ffffff;
-
-    & > options {
-      background-color:blue;
-    }
+  outline: none;
+  border: none;
+  width: 100%;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  color: #ffffff;
 `;
 
 export const StyledOption = styled.option`
-  color: ${(props) => (props.selected ? "lightgrey" : "black")};
+  color: ${(props) => (props.selected ? 'lightgrey' : 'black')};
   background-color: black;
 `;
 
 export const StyledLabel = styled.label`
   margin-bottom: 1rem;
   display: block;
-    font-family: 'Scto Grotesk A';
-    font-style: normal;
-    font-weight: bold;
-    font-size: 16px;
-    line-height: 20px;
-    color: #FFFFFF;
+  font-family: 'Scto Grotesk A';
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 20px;
+  color: #ffffff;
 `;
