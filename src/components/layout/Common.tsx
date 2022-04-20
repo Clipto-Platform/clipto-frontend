e9c5aef2c68f4ea4e461d14b25757414d7853665
Ryan Ng <ropher20@gmail.com>
2022-04-19 21:37:42 -0700

    got rid of black bar in explore

--- src/components/layout/Common.tsx
+++ src/components/layout/Common.tsx
@@ -11,7 +11,6 @@ export const PageWrapper = styled.div<{ blur?: boolean }>`
   min-height: 100vh;
   position: relative;
   overflow: hidden;
-  padding-bottom: 120px;
 `;
 export const PageContentWrapper = styled.div`
   padding: 0 32px;
