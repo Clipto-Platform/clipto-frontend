d7c3471641f32ce831cede3aa0db16a95aadea1e
rushikesh <rushikeshdhanwant@gmail.com>
2022-04-21 17:08:02 +0530

    added types for props

--- src/components/New/FrontPage.tsx
+++ src/components/New/FrontPage.tsx
@@ -3,14 +3,22 @@ import background1 from '../../assets/images/homepage/page1/background.png';
 import background2 from '../../assets/images/homepage/page2/background.png';
 import background3 from '../../assets/images/homepage/page3/background.png';
 
-export const Slides = styled.div`
+interface SlidesProps {
+  translate: any;
+}
+export const Slides = styled.div<SlidesProps>`
   display: flex;
   flex-direction: row;
   width: 300vw;
   transition: transform 1s;
   transform: ${(props) => `translateX(${props.translate}%)`};
 `;
-export const BackgroundWrapper = styled.div`
+
+interface BackgroundWrapperProps {
+  background: string;
+  translate: any;
+}
+export const BackgroundWrapper = styled.div<BackgroundWrapperProps>`
   background-image: url(${(props) => props.background});
   background-position: center center;
   background-size: cover;
