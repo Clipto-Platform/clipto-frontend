a3d72c708f060fb942091ffced8769e575a8973c
rushikesh <rushikeshdhanwant@gmail.com>
2022-04-22 17:19:57 +0530

    added conditional routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Web3Provider } from '@ethersproject/providers';
 import { Web3ReactProvider } from '@web3-react/core';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from 'styled-components';
 
@@ -19,7 +19,7 @@ import { OrdersPage } from './pages/Orders/Orders';
 import { SelectedOrderPage } from './pages/SelectedOrder/SelectedOrder';
 import { theme } from './styles/theme';
 import { BottomBar } from './components/BottomBar/BottomBar';
-
+import { useSelector } from 'react-redux';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider);
@@ -28,6 +28,7 @@ function getLibrary(provider: any): Web3Provider {
 }
 
 function App() {
+  const user = useSelector((state: any) => state.user);
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <ThemeProvider theme={theme}>
@@ -38,14 +39,14 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route index element={<HomePage />} />
             <Route path="explore" element={<ExplorePage />} />
-            <Route path="bounty" element={<BountyPage />} />
-            <Route path="onboarding/profile" element={<OnboardProfilePage />} />
-            <Route path="bountyDone" element={<BountyDone />} />
-            <Route path="invitation" element={<InvitationPage />} />
-            <Route path="mint" element={<MintPage />} />
-            <Route path="onboarding" element={<OnboardingPage />} />
-            <Route path="orders" element={<OrdersPage />} />
-            <Route path="orders/:creator/:requestId" element={<SelectedOrderPage />} />
+            {/* <Route path="bounty" element={<BountyPage />} /> */}
+            <Route path="onboarding/profile" element={user ? <OnboardProfilePage /> : <Navigate to="/" />} />
+            {/* <Route path="bountyDone" element={<BountyDone />} /> */}
+            {/* <Route path="invitation" element={<InvitationPage />} /> */}
+            {/* <Route path="mint" element={<MintPage />} /> */}
+            <Route path="onboarding" element={user ? <OnboardingPage /> : <Navigate to="/" />} />
+            <Route path="orders" element={user ? <OrdersPage /> : <Navigate to="/" />} />
+            <Route path="orders/:creator/:requestId" element={user ? <SelectedOrderPage /> : <Navigate to="/" />} />
             <Route path="creator/:creatorId" element={<BookingPage />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
