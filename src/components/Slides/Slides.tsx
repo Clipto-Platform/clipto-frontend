98d88d8a044805afb56bee2b068d742507fcb6ff
Ryan Ng <ropher20@gmail.com>
2022-05-03 15:48:48 -0700

    made slides moduler

--- src/components/Slides/Slides.tsx
+++ src/components/Slides/Slides.tsx
@@ -20,13 +20,31 @@ import { ContentWrapper, PageContentWrapper, PageWrapper } from '../layout/Commo
 import { HeaderContentGapSpacer, HeaderSpacer } from '../Header/Header';
 
 const Slides = (props: any) => {
-  const [page, setPage] = useState<number>(0);
-  const [slidesPosition, setSlidesPosition] = useState<number>(0);
-  const [slidePosition, setSlidePosition] = useState<Array<number>>([0, 0, -300]);
+  if (props.backgroundD.length != props.backgroundM.length || props.children.length != props.backgroundM.length)
+    console.warn('Slider props should have same length');
+  const NSlides = props.backgroundD.length;
+
+  const [firstSlide, setFirstSlide] = useState<number>(0);
+  const [lastSlide, setLastSlide] = useState<number>(NSlides - 1);
+  const [visibleSlide, setVisibleSlide] = useState<number>(0);
+  const [slidesPosition, setSlidesPosition] = useState<anny>(0);
+  const [slidePosition, setSlidePosition] = useState<Array<number>>([...Array(NSlides)].fill(0));
+
   const [clickEnabled, setClickEnabled] = useState(true);
   const [index, setIndex] = useState(0);
   const timeoutRef = useRef(null as any);
-
+  console.log(
+    'firstSlide:',
+    firstSlide,
+    'lastSlide:',
+    lastSlide,
+    'visibleSlide:',
+    visibleSlide,
+    'slidePosition:',
+    slidePosition,
+    'SlidesPosition:',
+    slidesPosition,
+  );
   useEffect(() => {
     resetTimeout();
     timeoutRef.current = setTimeout(rightClick, 8000);
@@ -42,22 +60,22 @@ const Slides = (props: any) => {
     resetTimeout();
 
     if (clickEnabled) {
-      let temp = slidePosition;
-      const round = Math.round(slidesPosition) % 100;
+      if (firstSlide == visibleSlide) {
+        let temp = slidePosition;
+        temp[lastSlide] = temp[lastSlide] - 100 * NSlides;
 
-      if (round == 0) {
-        temp[1] = slidePosition[1] - 300;
-        setPage(2);
-      } else if (round == 33 || round == -67) {
-        temp[0] = slidePosition[0] - 300;
-        setPage(1);
-      } else if (round == 67 || round == -33) {
-        temp[2] = slidePosition[2] - 300;
-        setPage(0);
+        setSlidePosition(temp);
+        setSlidesPosition(slidesPosition + 100 / NSlides);
+        setFirstSlide(lastSlide);
+        setVisibleSlide(lastSlide);
+        if (lastSlide == 0) setLastSlide(NSlides - 1);
+        else setLastSlide(lastSlide - 1);
+      } else {
+        if (visibleSlide == 0) setVisibleSlide(NSlides - 1);
+        else setVisibleSlide(visibleSlide - 1);
+        setSlidesPosition(slidesPosition + 100 / NSlides);
       }
 
-      setSlidePosition(temp);
-      setSlidesPosition(slidesPosition + 100 / 3);
       setClickEnabled(false);
       setTimeout(() => {
         setClickEnabled(true);
@@ -69,22 +87,22 @@ const Slides = (props: any) => {
     resetTimeout();
 
     if (clickEnabled) {
-      let temp = slidePosition;
-      const round = Math.round(slidesPosition) % 100;
+      if (lastSlide == visibleSlide) {
+        let temp = slidePosition;
+        temp[firstSlide] = temp[firstSlide] + 100 * NSlides;
 
-      if (round == 0) {
-        temp[2] = slidePosition[2] + 300;
-        setPage(1);
-      } else if (round == 33 || round == -67) {
-        temp[1] = slidePosition[1] + 300;
-        setPage(0);
-      } else if (round == 67 || round == -33) {
-        temp[0] = slidePosition[0] + 300;
-        setPage(2);
+        setSlidePosition(temp);
+        setSlidesPosition(slidesPosition - 100 / NSlides);
+        setLastSlide(firstSlide);
+        setVisibleSlide(firstSlide);
+        if (firstSlide == NSlides - 1) setFirstSlide(0);
+        else setFirstSlide(firstSlide + 1);
+      } else {
+        if (visibleSlide == NSlides - 1) setVisibleSlide(0);
+        else setVisibleSlide(visibleSlide + 1);
+        setSlidesPosition(slidesPosition - 100 / NSlides);
       }
 
-      setSlidePosition(temp);
-      setSlidesPosition(slidesPosition - 100 / 3);
       setClickEnabled(false);
       setTimeout(() => {
         setClickEnabled(true);
@@ -94,9 +112,12 @@ const Slides = (props: any) => {
   };
 
   const onOvalClick = (index: number) => {
-    if ((page < index && !(page == 0 && index == 2)) || (page == 2 && index == 0)) {
+    if ((visibleSlide < index && !(visibleSlide == 0 && index == NSlides)) || (visibleSlide == NSlides && index == 0)) {
       rightClick();
-    } else if ((page > index && !(page == 2 && index == 0)) || (page == 0 && index == 2)) {
+    } else if (
+      (visibleSlide > index && !(visibleSlide == NSlides && index == 0)) ||
+      (visibleSlide == 0 && index == NSlides)
+    ) {
       leftClick();
     }
   };
@@ -106,13 +127,13 @@ const Slides = (props: any) => {
       <Right onClick={rightClick} />
       <OvalSpacing>
         <Ovals>
-          {[...Array(3)].map((_, index) => (
-            <Oval page={page} index={index} onClick={() => onOvalClick(index)} />
+          {[...Array(NSlides)].map((_, index) => (
+            <Oval page={visibleSlide} index={index} onClick={() => onOvalClick(index)} />
           ))}
         </Ovals>
       </OvalSpacing>
-      <SlideContentWrapper translate={slidesPosition}>
-        {[...Array(3)].map((_, index) => (
+      <SlideContentWrapper translate={slidesPosition} NSlides={NSlides}>
+        {[...Array(NSlides)].map((_, index) => (
           <BackgroundWrapper
             translate={slidePosition[index]}
             index={index}
