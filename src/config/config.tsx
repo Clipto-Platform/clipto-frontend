cde906a15a4905bae5213c00c4a1df95a25a5b75
Dinesh Seervi <crvidinesh@gmail.com>
2022-05-19 17:04:53 +0530

    stage subgraph link

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -27,8 +27,7 @@ const configTest = {
   },
 
   rpcUrl: 'https://polygon-mumbai.g.alchemy.com/v2/VMBpFqjMYv2w-MWnc9df92w3R2TpMvSG',
-  // graphApi: 'https://api.thegraph.com/subgraphs/name/clipto-platform/clipto-subgraph-tstnet',
-  graphApi: 'https://api.thegraph.com/subgraphs/name/rushidhanwant/buisness',
+  graphApi: 'https://api.thegraph.com/subgraphs/name/clipto-platform/clipto-subgraph-tstnet',
   apiUrl: 'https://testapi.clipto.io',
 
   getContractExplorer: (address: string) => `https://mumbai.polygonscan.com/address/${address}`,
